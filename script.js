f08e41c5d86801c73326b24c2c3474e17fc17d35
Faizan Ahmed <153298469+Faizan-fzn@users.noreply.github.com>
2024-01-20 01:26:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ function firstWord(s) {
 
 	for(let i = 0; i < s.length; i++){
 		if(s.charAt(i) === " "){
+			if(!(result.length === 0)){
 			return result;
-		}
+		} else{
 		result += s.charAt(i);
+			}
 	}
 	return result;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
