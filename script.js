72bba6fd29681eae5d99064bf080d29488567fa5
Faizan Ahmed <153298469+Faizan-fzn@users.noreply.github.com>
2024-01-20 01:04:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
   // your code here
+	let result = "";
+
+	for(let i = 0; i < s.length; i++){
+		if(s.charAt(i) == " "){
+			return result;
+		}
+		result += s.charAt(i);
+	}
+	return result;
 }
 
 // Do not change the code below
